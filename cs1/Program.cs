17a7de454e51a5eb9bc53dee8189863b2ace7242
W <W@w.com>
2016-07-12 17:33:46 -0400

    Add project files.

---
+++ cs1/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace cs1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("cs1");
+            Console.Read();
+        }
+    }
+}
