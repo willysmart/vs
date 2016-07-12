7d4eddaf0659264e241147676fb73d20f07c3914
W <W@w.com>
2016-07-12 18:15:07 -0400

    b2_add_1

--- cs1/Program.cs
+++ cs1/Program.cs
@@ -13,6 +13,7 @@ namespace cs1
             Console.WriteLine("cs1");
             Console.WriteLine("a1_add_1");
             Console.WriteLine("b1_add_1");
+            Console.WriteLine("b2_add_1");
             Console.Read();
         }
     }
